31ca47e5f0d847455ee04df86555c8826abc6d4e
michalliu <michalliu@foxmail.com>
2017-11-23 15:10:31 +0800

    autogen android library

---
+++ autogenAndroid.sh
@@ -0,0 +1,5 @@
+cmake -DCMAKE_TOOLCHAIN_FILE=/Users/leoliu/Software/android-cmake/android.toolchain.cmake \
+      -DANDROID_NDK=/Users/leoliu/Library/Android/android-ndk-r10d                       \
+      -DCMAKE_BUILD_TYPE=Release                     \
+      -DANDROID_ABI="armeabi-v7a"          \
+      .
